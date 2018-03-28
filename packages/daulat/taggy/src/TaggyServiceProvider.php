355ba55761f7e16a0eaf33628bb23694375bbdf5
vagrant <vagrant@homestead>
2018-03-28 13:56:30 +0000

    first commit

---
+++ packages/daulat/taggy/src/TaggyServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Daulat\Taggy;
+
+use Illuminate\Support\ServiceProvider;
+
+class TaggyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+       $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
